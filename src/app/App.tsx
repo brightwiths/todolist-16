1cdfd5d69ffb1a161f5ddcc6da0eb0eaf63c6861
BriWS <brightwiths@gmail.com>
2021-09-09 19:58:40 +0300

    50%

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,8 @@ import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
 import {useSelector} from 'react-redux'
 import {AppRootStateType} from './store'
 import {RequestStatusType} from './app-reducer'
+import {Login} from "../features/Login/Login";
+import {Redirect, Route, Switch} from "react-router-dom";
 
 type PropsType = {
     demo?: boolean
@@ -16,7 +18,7 @@ function App({demo = false}: PropsType) {
     const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
     return (
         <div className="App">
-            <ErrorSnackbar />
+            <ErrorSnackbar/>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
@@ -27,10 +29,15 @@ function App({demo = false}: PropsType) {
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
-             { status === 'loading' &&  <LinearProgress /> }
+                {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList demo={demo}/>
+                <Switch>
+                    <Route exact path={'/'} render={() => <TodolistsList demo={demo}/>}/>
+                    <Route path={'/login'} render={() => <Login/>}/>
+                    <Route path={'/404'} render={() => <h1>404: PAGE NOT FOUND</h1>}/>
+                    <Redirect from={ '*' } to={'/404'} />
+                </Switch>
             </Container>
         </div>
     )
